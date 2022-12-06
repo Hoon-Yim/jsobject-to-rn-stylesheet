90a76a2be7ac03b0e692d7df2d97c3122ef1a77a
Hoon Yim <yimsh4507@gmail.com>
2022-12-06 14:00:26 -0500

    First Commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,39 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
 
 function App() {
+  const [text, setText] = useState("");
+  const [result, setResult] = useState("");
+
+  function handleSubmit(e) {
+    e.preventDefault();
+
+    let textArray = text.split(/\r?\n/);
+
+    textArray = textArray.map(text => {
+      const temp = text.replace(/\s/g, '');
+      if (temp.charAt(0) == "\"") {
+        return text.replace(/"/g, "").replace(".", "").replace(/-/g, "_");
+      } else {
+        return text;
+      }
+    })
+
+    let returningText = "";
+    for (let txt of textArray) {
+      returningText += txt + "\r\n";
+    }
+
+    setResult(returningText);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <a href="https://transform.tools/css-to-js">CSS to JS Object Converter</a>
+      <form onSubmit={handleSubmit}>
+        <textarea rows="50" cols="50" type="text" value={text} onChange={e => setText(e.target.value)}></textarea>
+        <button type="submit">Convert</button>
+        <pre>{result}</pre>
+      </form>
     </div>
   );
 }
